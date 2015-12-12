88ba109bb30c3a81c39b67ecc25baa5423637ab4
Phoenix787 <shiny777@bk.ru>
2015-12-12 17:58:45 +0500

    Add dependencies: jetty webapp, freemarker. Create handler Frontend and class Main for starting server

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import javax.servlet.ServletContext;
+
+/**
+ * Created by Ксения on 12.12.2015.
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Frontend frontend = new Frontend();
+
+        Server server = new Server(8080);
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(frontend), "/authform");
+
+        server.start();
+        server.join();
+    }
+}
